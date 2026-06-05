c790a9b9778fb0693ba2539f41da5864e63d91c5
shrmilamango <shyam098543@gmail.com>
2026-06-05 22:52:08 +0530

    Added Day-09 programs

---
+++ Day-09/36.c
@@ -0,0 +1,30 @@
+/* Write a program to Print hollow square 
+pattern. 
+***** 
+*   * 
+*   * 
+*   * 
+*****
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for(i = 1; i <= 5; i++) {
+        for(j = 1; j <= 5; j++) {
+
+            if(i == 1 || i == 5 || j == 1 || j == 5) {
+                printf("*");
+            }
+            else {
+                printf(" ");
+            }
+
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
