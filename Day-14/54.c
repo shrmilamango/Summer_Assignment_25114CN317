9bbe52c4c263686b71a581e772fcbae4989672ed
shrmilamango <shyam098543@gmail.com>
2026-06-10 23:31:43 +0530

    Added Day-14 Program

---
+++ Day-14/54.c
@@ -0,0 +1,29 @@
+// Write a program to Frequency of an element. 
+
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, key, count = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to find frequency: ");
+    scanf("%d", &key);
+
+    for(i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            count++;
+        }
+    }
+
+    printf("Frequency of %d = %d", key, count);
+
+    return 0;
+}
